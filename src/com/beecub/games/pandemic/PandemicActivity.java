c8c310c203b6f9f8a5639b70e9e9b7c44f1c9728
beecub <beecub@beecub.com>
2011-12-08 16:53:03 +0100

    - new worldmap

--- src/com/beecub/games/pandemic/PandemicActivity.java
+++ src/com/beecub/games/pandemic/PandemicActivity.java
@@ -1,7 +1,9 @@
 package com.beecub.games.pandemic;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
@@ -9,14 +11,20 @@ import android.view.WindowManager;
 public class PandemicActivity extends Activity {
 	
 	private static final String TAG = "beecub";
+	private PowerManager.WakeLock wl;
+
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // requesting to turn the title OFF
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  
         // making it full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "PandemicActivity");
+        
         // set our MainGamePanel as the View
         setContentView(new MainGamePanel(this));
         Log.d(TAG, "View added");
@@ -33,6 +41,17 @@ public class PandemicActivity extends Activity {
 		Log.d(TAG, "Stopping...");
 		super.onStop();
 	}
-    
+	
+	@Override
+    protected void onPause() {
+            super.onPause();
+            wl.release();
+    }
+
+    @Override
+    protected void onResume() {
+            super.onResume();
+            wl.acquire();
+    }    
     
 }
